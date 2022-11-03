28c0da578e5442c4fe5399605249cfc98761dfad
thehsadowte <103116337+thehsadowte@users.noreply.github.com>
2022-11-04 01:34:24 +0200

    task09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  button: document.querySelector('.change-color'),
+  span: document.querySelector('.color'),
+  widget: document.querySelector('.widget'),
+  bodyEl: document.querySelector('body'),
+};
+
+refs.button.addEventListener('click', onClickBtn);
+
+function onClickBtn() {
+  const color = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = color;
+
+  refs.span.textContent = color;
+}
